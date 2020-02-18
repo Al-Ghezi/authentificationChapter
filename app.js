cfe2a2b88c3263164dee682b4722c52f231b82ed
Osamah Al-Ghezi <Alghezi.osamah@gmail.com>
2020-02-18 15:02:00 +0100

    level up to level 5, using coockies, passport, passport-local passport-local-mongoose and sessions

--- app.js
+++ app.js
@@ -4,32 +4,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption"); //level 2 security
-// const md5 = require("md5") From level 3 security
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: "this is our secret",
+  resave: false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
+mongoose.set("useCreateIndex", true);
+
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);  //this is gonna hash and salt
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -43,54 +55,54 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function (req, res){
+  req.logout();
+  res.redirect("/");
+});
+//if you restart nodemon you will get loged out automatically or close chorme completelly
+
+
+
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
+  User.register({username:req.body.username}, req.body.password, function(err, user) {
+  if (err) {
+    console.log(err);
+    res.redirect("/register");
+  } else {
+    passport.authenticate("local")(req, res, function(){   // this part create the cookie and open the session
+    res.redirect("/secrets");
     });
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.render("secrets")
-      }
-    });
-
-  });
+  }
+});
 
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({
-      email: username
-    }, function(err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-            if (result === true) {
-              res.render("secrets");
-            }
-          });
-        }
-      }
+  const user = new User({
+    username : req.body.username,
+    password : req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
-  );
+  });
 });
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
